6b73ea02c876a1db627d9e892b5ce04fb706df3f
James Koonts <jkoontsiii@gmail.com>
2022-09-01 17:40:56 -0400

    init

--- src/main/java/config/usr/share/myq/startmyq.sh
+++ src/main/java/config/usr/share/myq/startmyq.sh
@@ -4,6 +4,8 @@ pkill -xf "python3 src/garagecontrollistener.py"
 pkill -xf "CheckHost Main"
 echo "processes stopped.."
 cd /usr/share/myq/
-exec -a 'CheckHost' java -cp /usr/share/myq/ Main
+#exec -a 'CheckHost' java -cp /usr/share/myq/ Main
+
+exec -a 'CheckHost' /usr/share/myq/myq/bin/Myq_Garage
 
 
