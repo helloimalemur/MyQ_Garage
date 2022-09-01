5595bec39ba9b3e9a970e4644c8ec3f65778dc24
James Koonts <jkoontsiii@gmail.com>
2022-09-01 14:23:39 -0400

    init

--- src/main/java/myq-archinstall.sh
+++ src/main/java/myq-archinstall.sh
@@ -13,7 +13,7 @@ cd ~
 git clone https://github.com/helloimalemur/MyQ_Garage
 cd MyQ_Garage/
 ./gradlew build
-cd MyQ_Garage/src/main/java/
+cd /src/main/java/
 makepkg -si --noconfirm
 sudo systemctl stop myq
 ##if configs had already exist they should be in home dir already
